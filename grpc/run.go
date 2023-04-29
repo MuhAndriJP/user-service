0bac7fa0ca5d8796fb01bde2607e5ac5f3f0c2d4
Muhammad Andri <andri@qasir.id>
2023-04-29 18:59:14 +0700

    move grpc run

---
+++ grpc/run.go
@@ -0,0 +1,29 @@
+package grpc
+
+import (
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/MuhAndriJP/user-service.git/grpc/user"
+	"github.com/MuhAndriJP/user-service.git/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func Run() {
+	lis, err := net.Listen("tcp", ":"+os.Getenv("port_grpc"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	} else {
+		log.Println("Connect GRPC to port " + os.Getenv("port_grpc"))
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServer(grpcServer, server.NewServerUser())
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-	"log"
-	"net"
-	"os"
-
-	pb "github.com/MuhAndriJP/user-service.git/grpc/user"
+	"github.com/MuhAndriJP/user-service.git/grpc"
 	"github.com/MuhAndriJP/user-service.git/repo/mysql"
-	"github.com/MuhAndriJP/user-service.git/server"
-	"google.golang.org/grpc"
 )
 
 func main() {
 	mysql.InitDB()
-	lis, err := net.Listen("tcp", ":"+os.Getenv("port_grpc"))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	} else {
-		log.Println("Connect GRPC to port " + os.Getenv("port_grpc"))
-	}
-
-	grpcServer := grpc.NewServer()
-	pb.RegisterUserServer(grpcServer, server.NewServerUser())
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	grpc.Run()
 }
